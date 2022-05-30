4606d68a7e660907bf5d09d1c508b4a3e0f4def1
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2022-05-30 21:01:34 +0100

    clear state

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -11,7 +11,7 @@ import openEye from "../../assets/icons/open-eye.svg"
 
 
 export const SignUp = () => {
-    const initialValue = { email: "", password: "", first_name:"", last_name:"" };
+    const initialValue = { email: "", password: "", first_name:"", last_name:"", confirm_password:"s" };
     const [formState, setFormState] = useState(initialValue);
     const [show,setShowPassword ] = useState(false)
     const [show2,setShowPassword2 ] = useState(false)
@@ -87,29 +87,29 @@ export const SignUp = () => {
                 <div className="lg:flex">
                     <div className="lg:w-1/2 w-full mr-3">
                     <label htmlFor="">First Name</label>
-                <InputField placeholder="john" name="first_name"  type="text" required={true} onChange={handleChange} icon={userIcon}  />
+                <InputField placeholder="john" name="first_name"  type="text" required={true} onChange={handleChange} icon={userIcon}  value={fornState.first_name}/>
                     </div>
 
                     <div className="lg:w-1/2 w-full mr-3">
                     <label htmlFor="">Last Name</label>
-                <InputField placeholder="doe"  type="text" name="last_name" required={true}  onChange={handleChange}  icon={userIcon}/>
+                <InputField placeholder="doe"  type="text" name="last_name" required={true}  onChange={handleChange}  icon={userIcon} value={formState.last_name}/>
                     </div>
                 </div>
                 <div className="mb">
                 <label htmlFor=""> Email</label>
-                <InputField placeholder="johndoe@gmail.com" name="email"  type="email" required={true} onChange={handleChange} icon={emailIcon}  />
+                <InputField placeholder="johndoe@gmail.com" name="email"  type="email" required={true} onChange={handleChange} icon={emailIcon} value={formState.email} />
                 </div>
                 <div className="mb-3">
                  </div>
                 <div className="lg:flex mb-3">
                 <div className="lg:w-1/2 w-full mr-3">
                     <label htmlFor=""> Password</label>
-                <InputField placeholder="**********" type={show ? "text":"password"} name="password" required={true} onChange={handleChange} icon={openEye} onToggle={togglePassword}  />
+                <InputField placeholder="**********" type={show ? "text":"password"} name="password" required={true} onChange={handleChange} icon={openEye} onToggle={togglePassword} value={formState.password} />
                 </div>
 
                 <div className="lg:w-1/2 w-full mr-3">
                     <label htmlFor=""> Confirm Password</label>
-                <InputField placeholder="**********" type={show2 ? "text":"password"} name="confirm_password" required={true} onChange={handleChange} icon={openEye} onToggle={togglePassword2} />
+                <InputField placeholder="**********" type={show2 ? "text":"password"} name="confirm_password" required={true} onChange={handleChange} icon={openEye} onToggle={togglePassword2} value={formState.confirm_password} />
                </div>
                 </div>
 
